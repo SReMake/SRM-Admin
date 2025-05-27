58afc8a86e75d61b6fdef8371ce8a611826caf37
yjh <langbiantianya@outlook.com>
2025-05-27 14:03:54 +0800

    fix: GlobalExceptionHandler

--- common/src/main/java/com/sreMake/common/exception/BusinessException.java
+++ common/src/main/java/com/sreMake/common/exception/BusinessException.java
@@ -4,6 +4,8 @@ package com.sreMake.common.exception;
  * 业务异常使用这个
  */
 public interface BusinessException {
-    /**实现该方法返回业务错误吗*/
+    /**
+     * 实现该方法返回业务错误吗
+     */
     int code();
 }
--- security/src/main/java/com/sreMake/security/exception/GlobalExceptionHandler.java
+++ security/src/main/java/com/sreMake/security/exception/GlobalExceptionHandler.java
@@ -3,29 +3,27 @@ package com.sreMake.security.exception;
 import com.sreMake.common.exception.BusinessException;
 import com.sreMake.common.exception.CanThrowException;
 import com.sreMake.common.result.ResponseResult;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseResult<?> handleException(Exception e, HttpServletResponse response) {
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseResult<?> handleRuntimeException(Exception e) {
         //业务异常
         if (e instanceof BusinessException) {
             return ResponseResult.error(((BusinessException) e).code(), e.getMessage());
         }
 
         // 处理其他异常
-        response.setStatus(HttpStatus.BAD_REQUEST.value());
         //可返回异常
         if (e instanceof CanThrowException) {
             return ResponseResult.error(400, e.getMessage());
@@ -34,6 +32,14 @@ public class GlobalExceptionHandler {
         if (e instanceof AccessDeniedException || e instanceof AuthenticationException) {
             throw (RuntimeException) e;
         }
+        //不可返回异常  记录日志
+        log.error(e.getMessage(), e);
+        return ResponseResult.error(-999, "操作失败，请稍后重试");
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseResult<?> handleException(Exception e) {
         //不可返回异常  记录日志
         log.error(e.getMessage(), e);
         return ResponseResult.error(-999, "操作失败，请稍后重试");
